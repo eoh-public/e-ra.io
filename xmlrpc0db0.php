1b3d8290769bf1de73704a78b63a6437ba622db6
Son Mac <son.lg@eoh.io>
2023-10-02 10:56:08 +0700

    fixed

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://home.e-ra.io</homePageLink>
+		<homePageLink>https://e-ra.io</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://home.e-ra.io/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://home.e-ra.io/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://home.e-ra.io/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://home.e-ra.io/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://home.e-ra.io/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://e-ra.io/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://e-ra.io/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://e-ra.io/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://e-ra.io/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://e-ra.io/wp-json/" />
 			</apis>
 	</service>
 </rsd>
